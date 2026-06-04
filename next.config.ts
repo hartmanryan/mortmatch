e3c3ac1d30ae1f6a6bf584c5bc55da88582152e5
Prop Knocks <propknocks@gmail.com>
2026-06-04 07:50:43 -0400

    Add /guthix native redirect in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/guthix',
+        destination: '/?ref=3a2546e2-74a6-40f4-8133-1f12388c1049&topic=See+How+Much+Cash+You+Can+Take+Out+Of+Your+House&chatslug=A+Cash+Out+Refinance',
+        permanent: false, // Use false (307) so you can easily change it later without browser caching
+      },
+    ];
+  },
 };
 
 export default nextConfig;
