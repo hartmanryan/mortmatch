c8f570a1f4e270d8bab5e2119eeaa024bd0266a2
Prop Knocks <propknocks@gmail.com>
2026-06-04 08:16:49 -0400

    chore: update /guthix redirect to point to Jordan's correct database ID

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/guthix',
-        destination: '/?ref=3a2546e2-74a6-40f4-8133-1f12388c1049&topic=See+How+Much+Cash+You+Can+Take+Out+Of+Your+House&chatslug=A+Cash+Out+Refinance',
+        destination: '/?ref=0dcde69a-4e93-4fb1-b445-18420396aa72&topic=See+How+Much+Cash+You+Can+Take+Out+Of+Your+House&chatslug=A+Cash+Out+Refinance',
         permanent: false, // Use false (307) so you can easily change it later without browser caching
       },
     ];
